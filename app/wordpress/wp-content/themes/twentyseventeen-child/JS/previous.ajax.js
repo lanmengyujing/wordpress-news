cfa23f526d7684e783a9f0a29cccbf4ff1c66666
Jing Liu <jing.liu@rea-group.com>
2019-12-22 21:00:35 +1100

    get data from PHP code into JS files

--- app/wordpress/wp-content/themes/twentyseventeen-child/JS/previous.ajax.js
+++ app/wordpress/wp-content/themes/twentyseventeen-child/JS/previous.ajax.js
@@ -3,6 +3,19 @@
  */
 
 (function($){
+    const postId = postdata.post_id;
+    const theme_uri = postdata.theme_uri;
+    const rest_url = postdata.rest_url;
+    
     $('.load-previous a').attr( 'href', 'javascript:void(0)');
 
+    function get_previous_post(){
+        $.ajax({
+            dataType: "json",
+            url: rest_url + "posts/" + postId   
+        }).done((response)=>{
+            console.log(response);
+        })
+    }
+    $('.load-previous a').on('click',get_previous_post )
 })(jQuery);
