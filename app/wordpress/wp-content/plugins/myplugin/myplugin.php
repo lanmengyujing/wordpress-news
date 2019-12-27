7480565c6ae3c77b67d75d00df2289da8805f33f
Jing Liu <jing.liu@rea-group.com>
2019-12-28 10:43:05 +1100

    add plugin

---
+++ app/wordpress/wp-content/plugins/myplugin/myplugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name:       My Plugin
+Description:       A plugin for
+Version:           0.3
+Author:            Automattic
+Author URI:        http://automattic.com/
+License:           GPLv2 or later
+*/
