adf189a4207c1ec5bf5f9dac1d99ebf66b7c7490
Jing Liu <jing.liu@rea-group.com>
2019-12-28 13:02:03 +1100

    add settings for plugin

--- app/wordpress/wp-content/plugins/myplugin/myplugin.php
+++ app/wordpress/wp-content/plugins/myplugin/myplugin.php
@@ -15,3 +15,28 @@ if ( !defined( 'ABSPATH' ) ) {
 
 require_once plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php';
 require_once plugin_dir_path( __FILE__ ) . 'admin/settings-page.php';
+
+// register plugin settings
+function myplugin_register_settings() {
+	/*
+	register_setting( 
+		string   $option_group, 
+		string   $option_name,  //get option from database
+		callable $sanitize_callback
+	);
+	*/
+	register_setting( 
+		'myplugin_options', 
+		'myplugin_options', 
+		'myplugin_callback_validate_options' 
+	); 
+
+}
+// validate plugin settings
+function myplugin_validate_options($input) {
+	// todo: add validation functionality..
+	return $input;
+	
+}
+
+add_action( 'admin_init', 'myplugin_register_settings' );
