c410c78119d0e7e0f2315344a0b0aa72b77cab70
Jing Liu <jing.liu@rea-group.com>
2019-12-28 11:18:01 +1100

    add basic files for plugin

--- app/wordpress/wp-content/plugins/myplugin/myplugin.php
+++ app/wordpress/wp-content/plugins/myplugin/myplugin.php
@@ -1,9 +1,15 @@
 <?php
 /*
 Plugin Name:       My Plugin
-Description:       A plugin for
-Version:           0.3
-Author:            Automattic
-Author URI:        http://automattic.com/
+Description:       A plugin for practice
+Version:           0.0.1
+Author:            Jing Liu
+Author URI:        http://localhost.com/
 License:           GPLv2 or later
 */
+
+
+// exit if file is called directly
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
